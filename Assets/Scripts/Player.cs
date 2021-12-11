f1d350bb249a02e867c52e7a271890a09a18ed1a
Owais <25351744+owais635@users.noreply.github.com>
2021-12-11 04:43:54 -0500

    make player move left/right

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveForce = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movePlayer();
+    }
+
+    void movePlayer()
+    {
+        float movementX = Input.GetAxisRaw("Horizontal");
+        transform.position +=
+            new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
     }
 }
