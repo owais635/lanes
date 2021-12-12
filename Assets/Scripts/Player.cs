5c7f162aef5598d05e308c649ccad0f592f11f53
Owais <25351744+owais635@users.noreply.github.com>
2021-12-12 04:47:39 -0500

    make player move up; and camera follow

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D myRigidBody;
+
+    private float speed = 5;
+
     public float moveForce = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,5 +27,12 @@ public class Player : MonoBehaviour
         float movementX = Input.GetAxisRaw("Horizontal");
         transform.position +=
             new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void FixedUpdate()
+    {
+        // move player upwards
+        myRigidBody.AddForce(new Vector2(0, speed));
+        // myRigidBody.velocity = new  Vector2(0, speed);
     }
 }
